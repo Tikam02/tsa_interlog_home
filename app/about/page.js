c34bcee6e6d87cd3503424e9f2f23db61f83a1ba
Tikam Singh Alma <timonalma81@gmail.com>
2025-01-26 23:22:48 +0530

    landing page

---
+++ app/about/page.js
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+    return (
+      <div className="min-h-screen">
+        <h1 className="text-4xl font-bold text-center mt-20">About Us</h1>
+        {/* Add your about content here */}
+      </div>
+    );
+  }

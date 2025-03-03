892c815877af6e2a7fdbe70447cd8edd08573f00
Tikam Singh Alma <timonalma81@gmail.com>
2025-03-03 19:00:12 +0530

    updates

--- app/contact/page.js
+++
@@ -1,8 +0,0 @@
-export default function ContactPage() {
-    return (
-      <div className="min-h-screen">
-        <h1 className="text-4xl font-bold text-center mt-20">Contact Us</h1>
-        {/* Add your contact content here */}
-      </div>
-    );
-  }
--- app/services/page.js
+++
@@ -1,8 +0,0 @@
-export default function ServicesPage() {
-    return (
-      <div className="min-h-screen">
-        <h1 className="text-4xl font-bold text-center mt-20">Our Services</h1>
-        {/* Add your services content here */}
-      </div>
-    );
-  }
